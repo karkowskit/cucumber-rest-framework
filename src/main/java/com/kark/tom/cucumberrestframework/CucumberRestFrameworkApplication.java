7b79fcc1432d81f209e1f825354efb2e550aa6e7
Tom Kark <karkowski@o2.pl>
2020-01-17 18:36:28 +0100

    initial commit

---
+++ src/main/java/com/kark/tom/cucumberrestframework/CucumberRestFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.kark.tom.cucumberrestframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberRestFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberRestFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kark/tom/cucumberrestframework/CucumberRestFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kark.tom.cucumberrestframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberRestFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
